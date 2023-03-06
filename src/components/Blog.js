dd389c0882a92708215560fbc72236d5f65bdb2a
luisscodev <luisscodev@gmail.com>
2023-03-05 22:13:44 -0500

    my first article

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <h1>Blog</h1>
+    <>
+      <h1>Blog</h1>
+      <section>
+        <article>
+          <h2>My firts article</h2>
+          <p>Let's go</p>
+        </article>
+      </section>
+    </>
   )
 }
 
